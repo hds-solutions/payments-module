83f142aa9d94808b682c5e16271bda9d70f8db31
Hermann D. Schimpf <hschimpf@hds-solutions.net>
2021-04-28 22:44:12 -0400

    Payment relations

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -2,6 +2,13 @@
 
 namespace HDSSolutions\Finpar\Models;
 
+use HDSSolutions\Finpar\Traits\HasPartnerable;
+
 class Payment extends X_Payment {
+    use HasPartnerable;
+
+    public function currency() {
+        return $this->belongsTo(Currency::class);
+    }
 
 }
