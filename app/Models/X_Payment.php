43385d507a2023c68909a807fa49e52e480d19ae
Miguel Romero <mromero1346@gmail.com>
2021-04-28 23:05:23 -0400

    Add payment methods

--- app/Models/X_Payment.php
+++ app/Models/X_Payment.php
@@ -5,9 +5,25 @@ namespace HDSSolutions\Finpar\Models;
 use HDSSolutions\Finpar\Traits\BelongsToCompany;
 use Illuminate\Database\Eloquent\Builder;
 
-abstract class X_Payment extends Base\Model {
+abstract class X_Payment extends Base\Model
+{
     use BelongsToCompany;
 
+    const PAYMENT_TYPE_Cash = 'C';
+    const PAYMENT_TYPE_Credit = 'CR';
+    const PAYMENT_TYPE_Check = 'CH';
+    const PAYMENT_TYPE_CreditNote = 'CRD';
+    const PAYMENT_TYPE_FastPay = 'FP';
+    const PAYMENT_TYPE_Card = 'CAR';
+    const PAYMENT_TYPES = [
+        self::PAYMENT_TYPE_Cash => 'payment::payment.payment_type.cash',
+        self::PAYMENT_TYPE_Credit => 'payment::payment.payment_type.credit',
+        self::PAYMENT_TYPE_Check => 'payment::payment.payment_type.check',
+        self::PAYMENT_TYPE_CreditNote => 'payment::payment.payment_type.credit_note',
+        self::PAYMENT_TYPE_FastPay => 'payment::payment.payment_type.fast_pay',
+        self::PAYMENT_TYPE_Card => 'payment::payment.payment_type.card',
+    ];
+    
     protected $fillable = [
         'currency_id',
         'partnerable_type',
---
+++ resources/lang/en/payment.php
@@ -0,0 +1,22 @@
+<?php return [
+
+    'payment_id'       => [
+        'Payment',
+        '_' => 'Payment',
+        '?' => 'Payment help text',
+    ],
+
+    'payment_type'     => [
+        'Cash Type',
+        '_' => 'Cash Type',
+        '?' => 'Cash Type help text',
+
+        'cash'           => 'Transfer In',
+        'credit'          => 'Transfer Out',
+        'check'            => 'Difference',
+        'credit_note'           => 'Credit Note',
+        'fast_pay' => 'Employee Anticipation',
+        'card'       => 'General Expense',
+    ],
+
+];
