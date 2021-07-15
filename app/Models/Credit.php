85bc337b79b1e9488337a58afc046a77bc17ebd9
Hermann D. Schimpf <hschimpf@hds-solutions.net>
2021-07-15 18:05:06 -0400

    Next document_number method on Credit payment type

--- app/Models/Credit.php
+++ app/Models/Credit.php
@@ -4,4 +4,9 @@ namespace HDSSolutions\Finpar\Models;
 
 class Credit extends X_Credit {
 
+    public static function nextDocumentNumber():string {
+        // return next document number
+        return str_increment(self::join('payments', 'payments.id', 'credits.id')->max('document_number') ?? null);
+    }
+
 }
