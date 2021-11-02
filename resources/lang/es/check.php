64ac192124aec30f874393a1159ef50e224e4aed
Hermann D. Schimpf <hschimpf@hds-solutions.net>
2021-11-02 14:44:12 -0300

    Translations of Check and Payment

---
+++ resources/lang/es/check.php
@@ -0,0 +1,75 @@
+<?php return [
+
+    'details' => [
+        'Details',
+        '_' => 'Details',
+        '?' => 'Details help text',
+    ],
+
+    'transacted_at' => [
+        'Fecha',
+        '_' => 'Fecha',
+        '?' => 'Fecha help text',
+    ],
+
+    'document_number' => [
+        'Número de documento',
+        '_' => 'Número de documento',
+        '?' => 'Número de documento help text',
+    ],
+
+    'bank_id'  => [
+        'Banco',
+        '_' => 'Banco',
+        '?' => 'Banco help text',
+    ],
+
+    'bank_account_id'   => [
+        'Depositado en',
+        '_' => 'Depositado en',
+        '?' => 'Depositado en help text',
+    ],
+
+    'account_holder'  => [
+        'Titular de la Cuenta',
+        '_' => 'Titular de la Cuenta',
+        '?' => 'Titular de la Cuenta help text',
+    ],
+
+    'due_date' => [
+        'Fecha de Vencimiento',
+        '_' => 'Fecha de Vencimiento',
+        '?' => 'Fecha de Vencimiento help text',
+    ],
+
+    'partnerable_id'    => [
+        'Partner',
+        '_' => 'Partner',
+        '?' => 'Partner help text',
+    ],
+
+    'payment_amount'    => [
+        'Monto',
+        '_' => 'Monto',
+        '?' => 'Monto help text',
+    ],
+
+    'is_deposited'      => [
+        'Depositado',
+        '_' => 'Depositado',
+        '?' => 'Depositado help text',
+    ],
+
+    'is_cashed'      => [
+        'Cobrado',
+        '_' => 'Cobrado',
+        '?' => 'Cobrado help text',
+    ],
+
+    'is_paid'      => [
+        'Pagado',
+        '_' => 'Pagado',
+        '?' => 'Pagado help text',
+    ],
+
+];
